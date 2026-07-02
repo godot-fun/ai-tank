ad85cd110dcd8c406d78a57887d229bcb6f27a29
jaysunxiao <jaysunxiao@gmail.com>
2026-07-02 17:40:14 +0800

    update

--- script/tile/Eagle.gd
+++ script/tile/Eagle.gd
@@ -10,6 +10,10 @@ static var egale_first_grid_pos := Vector2i((TileConfig.MAP_GRID_WIDTH - 2) / 2,
 static var egale_sprite: Sprite2D
 
 static func create_base() -> void:
+	create_eagle()
+	create_base_bricks()
+
+static func create_eagle() -> void:
 	egale_sprite = Sprite2D.new()
 	egale_sprite.texture = load(TEXTURE)
 	egale_sprite.position = TankConfig.grid_to_world(egale_first_grid_pos, Vector2i.ONE) + TileConfig.ONE_GRID_SIZE * 0.5
@@ -20,22 +24,20 @@ static func create_base() -> void:
 
 	var parent: Node = (Engine.get_main_loop() as SceneTree).current_scene
 	parent.add_child(egale_sprite)
-
+	
 	for x in range(2):
 		for y in range(2):
-			TileHelper.create_tile(TileConfig.eagle, egale_first_grid_pos + Vector2i(x, y))
+			TileHelper.create_tile(TileConfig.eagle, egale_first_grid_pos + Vector2i(x, y))	
+	pass
 
-	_create_base_bricks()
-
-
-static func _create_base_bricks() -> void:
+static func create_base_bricks() -> void:
 	var pos := egale_first_grid_pos
 	for x in range(-1, 3):
 		TileHelper.create_tile(TileConfig.brick_wall_eagle, pos + Vector2i(x, -1))
 	for y in range(2):
 		TileHelper.create_tile(TileConfig.brick_wall_eagle, pos + Vector2i(-1, y))
 		TileHelper.create_tile(TileConfig.brick_wall_eagle, pos + Vector2i(2, y))
-
+	pass
 
 
 func destroy() -> void:
