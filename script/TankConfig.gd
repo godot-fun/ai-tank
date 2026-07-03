b15f8234da49ad4d692a50049b3f4159698a532e
jaysunxiao <jaysunxiao@gmail.com>
2026-07-03 21:22:39 +0800

    update

--- script/TankConfig.gd
+++ script/TankConfig.gd
@@ -11,7 +11,7 @@ const AUDIO_TANK_DEATH_ENEMY := "res://audio/sfx/tank-death/02.wav"
 const AUDIO_TANK_MOVE := "res://audio/sfx/tank/tank_move_fade.wav"
 
 const AUDIO_BULLET_HIT_BULLET := "res://audio/sfx/emp-hit/01.wav"
-const AUDIO_BULLET_HIT_TANK := "res://audio/sfx/tank/bullet_hit_tank.wav  "
+const AUDIO_BULLET_HIT_TANK := "res://audio/sfx/tank/bullet_hit_tank.wav"
 const AUDIO_BULLET_HIT_STEEL := "res://audio/sfx/bullet-hit-steel/01.wav"
 const AUDIO_BULLET_HIT_BRICK := "res://audio/sfx/bullet-hit-brick/01.wav"
 
