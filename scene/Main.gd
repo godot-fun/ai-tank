cbed20432c11f634ecbb4b220440e5f2fab63a19
jaysunxiao <jaysunxiao@gmail.com>
2026-07-04 18:37:42 +0800

    update

--- scene/Main.gd
+++ scene/Main.gd
@@ -6,10 +6,9 @@ const HOME_SCENE_PATH := "res://scene/Home.tscn"
 @onready var opening_video: VideoStreamPlayer = $OpeningVideo
 @onready var enter_prompt: Label = $EnterPrompt
 
-var _waiting_for_enter := false
-
 
 func _ready() -> void:
+	set_process_input(false)
 	Audio.play_music(BgmConfig.BGM_OPENING_DEMO_PART1)
 	play_opening_video()
 	pass
@@ -25,7 +24,6 @@ func play_opening_video() -> void:
 		opening_video.stop()
 
 	await show_enter_prompt()
-	_waiting_for_enter = true
 	pass
 
 
@@ -37,6 +35,8 @@ func show_enter_prompt() -> void:
 	fade_tween.tween_property(enter_prompt, "modulate:a", 1.0, 0.5)
 	await fade_tween.finished
 
+	set_process_input(true)
+
 	var pulse_tween := create_tween().set_loops()
 	pulse_tween.tween_property(enter_prompt, "modulate:a", 0.4, 0.8)
 	pulse_tween.tween_property(enter_prompt, "modulate:a", 1.0, 0.8)
@@ -44,16 +44,14 @@ func show_enter_prompt() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if not _waiting_for_enter:
-		return
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		get_viewport().set_input_as_handled()
+		set_process_input(false)
 		on_enter_pressed()
 	pass
 
 
 func on_enter_pressed() -> void:
-	_waiting_for_enter = false
 	Audios.play("res://audio/sfx/ui-confirm/01.wav")
 	await SceneHelper.async_change_scene_to_file(HOME_SCENE_PATH)
 	pass
