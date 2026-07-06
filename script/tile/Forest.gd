243d13ae0ac6865df00acf56e3ef69f7931c15c7
jaysunxiao <jaysunxiao@gmail.com>
2026-07-06 16:12:10 +0800

    update

--- script/tile/Forest.gd
+++ script/tile/Forest.gd
@@ -1,8 +1,22 @@
 extends Tile
 class_name Forest
 
+const SWAY_SHADER := "res://shader/forest_sway.gdshader"
+
+static var sway_material: ShaderMaterial
+
+
 func start() -> void:
 	z_index = 1
+	setup_sway_material()
+	pass
+
+
+func setup_sway_material() -> void:
+	if sway_material == null:
+		sway_material = ShaderMaterial.new()
+		sway_material.shader = load(SWAY_SHADER)
+	sprite.material = sway_material
 	pass
 
 
