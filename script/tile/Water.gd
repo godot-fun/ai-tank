10f3fee44d76dd597eb12b33341968700588873f
jaysunxiao <jaysunxiao@gmail.com>
2026-07-06 16:05:34 +0800

    update

--- script/tile/Water.gd
+++ script/tile/Water.gd
@@ -1,6 +1,24 @@
 ﻿extends Tile
 class_name Water
 
+const WAVE_SHADER := "res://shader/water_wave.gdshader"
+
+static var wave_material: ShaderMaterial
+
+
+func start() -> void:
+	setup_wave_material()
+	pass
+
+
+func setup_wave_material() -> void:
+	if wave_material == null:
+		wave_material = ShaderMaterial.new()
+		wave_material.shader = load(WAVE_SHADER)
+	sprite.material = wave_material
+	pass
+
+
 func blocks_tank() -> bool:
 	return true
 
