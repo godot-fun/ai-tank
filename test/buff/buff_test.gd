ab2e67a43794915e0bb08a21db65900db314e89b
jaysunxiao <jaysunxiao@gmail.com>
2026-07-09 19:18:27 +0800

    update

--- test/buff/buff_test.gd
+++ test/buff/buff_test.gd
@@ -4,4 +4,6 @@ extends Node2D
 func _ready() -> void:
 	TankHelper.create_tank(TankConfig.my_tank, Vector2i(10, TileConfig.MAP_GRID_HEIGHT))
 	TankHelper.create_tank(TankConfig.only_fire_enemy, Vector2i(10, 0))
+	
+	BuffHelper.create_buff(BuffConfig.bullet_size_buff, Vector2i(4, 4))
 	pass
