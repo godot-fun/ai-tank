6c321b7bda19f098b5c86889e1d7c0060a1ce6ba
jaysunxiao <jaysunxiao@gmail.com>
2026-07-09 21:11:29 +0800

    update

--- script/buff/BulletSpeedBuff.gd
+++ script/buff/BulletSpeedBuff.gd
@@ -2,3 +2,8 @@
 extends Buff
 
 
+const MAX_BULLET_SPEED: float = 2000
+
+func trigger(tank: Tank) -> void:
+	tank.bullet_speed = min(tank.bullet_speed + 200.0, MAX_BULLET_SPEED)
+	pass
--- test/buff/buff_test.gd
+++ test/buff/buff_test.gd
@@ -6,4 +6,10 @@ func _ready() -> void:
 	TankHelper.create_tank(TankConfig.only_fire_enemy, Vector2i(10, 0))
 	
 	BuffHelper.create_buff(BuffConfig.bullet_size_buff, Vector2i(4, 4))
+	BuffHelper.create_buff(BuffConfig.bullet_size_buff, Vector2i(4, 6))
+	BuffHelper.create_buff(BuffConfig.bullet_size_buff, Vector2i(4, 8))
+	
+	BuffHelper.create_buff(BuffConfig.bullet_speed_buff, Vector2i(1, 4))
+	BuffHelper.create_buff(BuffConfig.bullet_speed_buff, Vector2i(1, 6))
+	BuffHelper.create_buff(BuffConfig.bullet_speed_buff, Vector2i(1, 8))
 	pass
