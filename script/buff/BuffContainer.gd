6922b40733f7ce2dc27bf10248676bf9a378abfd
jaysunxiao <jaysunxiao@gmail.com>
2026-07-19 15:25:32 +0800

    update

--- script/buff/BuffContainer.gd
+++ script/buff/BuffContainer.gd
@@ -7,6 +7,13 @@ func add_buff(buff: IBuff) -> void:
 	buffs.append(buff)
 	pass
 
+func remove_buff(buff: IBuff) -> bool:
+	var index := buffs.find(buff)
+	if index == -1:
+		return false
+	buffs.remove_at(index)
+	return true
+
 func get_buffs_by_type(buff_type: int) -> Array[IBuff]:
 	var result: Array[IBuff] = []
 	for buff in buffs:
---
+++ test/unit/buff_manager_test.gd
@@ -0,0 +1,47 @@
+﻿static func buff_container_add_test() -> void:
+	var container := BuffContainer.new()
+	assert(container.buffs.is_empty())
+
+	var buff := BulletSizeBuff.new()
+	container.add_buff(buff)
+	assert(container.buffs.size() == 1)
+	assert(container.buffs[0] == buff)
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SIZE) == 1)
+
+	var same_type := BulletSizeBuff.new()
+	container.add_buff(same_type)
+	assert(container.buffs.size() == 2)
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SIZE) == 2)
+
+	var speed_buff := BulletSpeedBuff.new()
+	container.add_buff(speed_buff)
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SPEED) == 1)
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SIZE) == 2)
+
+	var size_buffs := container.get_buffs_by_type(IBuff.BuffType.BULLET_SIZE)
+	assert(size_buffs.size() == 2)
+	assert(size_buffs[0] == buff)
+	assert(size_buffs[1] == same_type)
+
+
+static func buff_container_remove_test() -> void:
+	var container := BuffContainer.new()
+	var buff1 := BulletSizeBuff.new()
+	var buff2 := BulletSizeBuff.new()
+	container.add_buff(buff1)
+	container.add_buff(buff2)
+
+	assert(container.remove_buff(buff1))
+	assert(container.buffs.size() == 1)
+	assert(container.buffs[0] == buff2)
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SIZE) == 1)
+
+	var size_buffs := container.get_buffs_by_type(IBuff.BuffType.BULLET_SIZE)
+	assert(size_buffs.size() == 1)
+	assert(size_buffs[0] == buff2)
+
+	assert(container.remove_buff(buff2))
+	assert(container.buffs.is_empty())
+	assert(container.buff_type_of_size(IBuff.BuffType.BULLET_SIZE) == 0)
+
+	assert(!container.remove_buff(buff2))
