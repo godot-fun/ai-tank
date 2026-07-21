b40d59102fa2fb0c2e3c696651c675905403c699
jaysunxiao <jaysunxiao@gmail.com>
2026-07-21 21:50:02 +0800

    update

--- test/tank/single_enemy_test.gd
+++ test/tank/single_enemy_test.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _ready() -> void:
 	TankHelper.create_tank(TankConfig.my_tank, Eagle.player_tank_start_grid_pos)
-	TankHelper.create_tank(TankConfig.only_fire_enemy, Vector2i(10, 0))
+	TankHelper.create_tank_with_buffs(TankConfig.only_fire_enemy, Vector2i(10, 0), [BulletSizeBuff.new()])
 	
 	BuffHelper.create_buff(BuffConfig.tank_hp_buff, Vector2i(Eagle.player_tank_start_grid_pos.x, 4))
 	BuffHelper.create_buff(BuffConfig.tank_hp_buff, Vector2i(Eagle.player_tank_start_grid_pos.x, 6))
