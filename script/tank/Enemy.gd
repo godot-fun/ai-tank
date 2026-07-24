bba8210d655f8a570d37c244d4398e3d1a6ffda4
jaysunxiao <jaysunxiao@gmail.com>
2026-07-24 10:50:51 +0800

    update

---
+++ script/tank/Enemy.gd
@@ -0,0 +1,23 @@
+﻿extends Tank
+class_name Enemy
+
+func play_enter_animation() -> void:
+	visible = false
+	moving = true
+	update_facing(Vector2i.DOWN)
+
+	var target_pos := global_position
+	global_position = Vector2(
+		target_pos.x,
+		-grid_size.y * TileConfig.TILE_SIZE * 0.5 - TileConfig.TILE_SIZE,
+	)
+	visible = true
+
+	var duration := global_position.distance_to(target_pos) / speed
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", target_pos, duration)
+	tween.finished.connect(func() -> void:
+		moving = false
+		global_position = TileConfig.grid_to_world(grid_pos, grid_size)
+	)
+	pass
--- script/tank/EnemyEasy.gd
+++ script/tank/EnemyEasy.gd
@@ -1,4 +1,4 @@
-extends Tank
+extends Enemy
 class_name EnemyEasy
 
 const AI_THINK_INTERVAL := 0.6
@@ -36,25 +36,3 @@ func pick_move_direction() -> Vector2i:
 	if randf() < 0.35:
 		return pick_random_not_blocked_direction()
 	return pick_direction_toward(player.grid_pos)
-
-
-func play_enter_animation() -> void:
-	visible = false
-	moving = true
-	update_facing(Vector2i.DOWN)
-
-	var target_pos := global_position
-	global_position = Vector2(
-		target_pos.x,
-		-grid_size.y * TileConfig.TILE_SIZE * 0.5 - TileConfig.TILE_SIZE,
-	)
-	visible = true
-
-	var duration := global_position.distance_to(target_pos) / speed
-	var tween := create_tween()
-	tween.tween_property(self, "global_position", target_pos, duration)
-	tween.finished.connect(func() -> void:
-		moving = false
-		global_position = TileConfig.grid_to_world(grid_pos, grid_size)
-	)
-	pass
