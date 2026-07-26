c37d2f02f47e8f73dee77bf2bb7a11ee65a45f15
jaysunxiao <jaysunxiao@gmail.com>
2026-07-26 09:03:49 +0800

    update

--- script/tank/PartnerTank.gd
+++ script/tank/PartnerTank.gd
@@ -59,18 +59,75 @@ func fire() -> void:
 	super.fire()
 
 
+## 朝向基地且直线上无阻挡时禁止开火。
 func is_aiming_at_home() -> bool:
+	if not is_facing_home():
+		return false
+
 	var home := Eagle.egale_first_grid_pos
 	var home_max := home + Vector2i.ONE
 
-	var x_overlap := grid_pos.x <= home_max.x and grid_pos.x + grid_size.x - 1 >= home.x
-	var y_overlap := grid_pos.y <= home_max.y and grid_pos.y + grid_size.y - 1 >= home.y
+	for ox in range(grid_size.x):
+		for oy in range(grid_size.y):
+			var cell := grid_pos + Vector2i(ox, oy) + facing
+			# 下一格仍在自身内，说明不是朝向前沿
+			if cell.x >= grid_pos.x and cell.x < grid_pos.x + grid_size.x \
+				and cell.y >= grid_pos.y and cell.y < grid_pos.y + grid_size.y:
+				continue
+
+			while TileHelper.is_cell_in_bounds(cell):
+				if cell.x >= home.x and cell.x <= home_max.x and cell.y >= home.y and cell.y <= home_max.y:
+					return not has_enemy_in_facing()
+
+				var tile := TileHelper.get_tile(cell)
+				if tile != null and tile.blocks_bullet():
+					return false
+
+				cell += facing
+
+	return false
+
+
+## 是否直面基地。
+func is_facing_home() -> bool:
+	var home := Eagle.egale_first_grid_pos
+	var home_max := home + Vector2i.ONE
+	var self_max := grid_pos + grid_size - Vector2i.ONE
+	var x_overlap := grid_pos.x <= home_max.x and self_max.x >= home.x
+	var y_overlap := grid_pos.y <= home_max.y and self_max.y >= home.y
 
 	match facing:
 		Vector2i.LEFT:
 			return grid_pos.x > home_max.x and y_overlap
 		Vector2i.RIGHT:
-			return grid_pos.x + grid_size.x - 1 < home.x and y_overlap
+			return self_max.x < home.x and y_overlap
+		Vector2i.UP:
+			return grid_pos.y > home_max.y and x_overlap
 		Vector2i.DOWN:
-			return x_overlap and grid_pos.y + grid_size.y - 1 <= home_max.y
+			return self_max.y < home.y and x_overlap
+	return false
+
+
+## 朝向上是否有敌方坦克。
+func has_enemy_in_facing() -> bool:
+	var self_max := grid_pos + grid_size - Vector2i.ONE
+
+	for tank in TankHelper.tanks:
+		if not tank.is_alive_enemy():
+			continue
+		var t_min := tank.grid_pos
+		var t_max := tank.grid_pos + tank.grid_size - Vector2i.ONE
+		match facing:
+			Vector2i.LEFT:
+				if t_max.x < grid_pos.x and t_min.y <= self_max.y and t_max.y >= grid_pos.y:
+					return true
+			Vector2i.RIGHT:
+				if t_min.x > self_max.x and t_min.y <= self_max.y and t_max.y >= grid_pos.y:
+					return true
+			Vector2i.UP:
+				if t_max.y < grid_pos.y and t_min.x <= self_max.x and t_max.x >= grid_pos.x:
+					return true
+			Vector2i.DOWN:
+				if t_min.y > self_max.y and t_min.x <= self_max.x and t_max.x >= grid_pos.x:
+					return true
 	return false
