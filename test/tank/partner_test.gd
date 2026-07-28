27d6472d8fd9e8fce9bfd07008084b5915c95463
jaysunxiao <jaysunxiao@gmail.com>
2026-07-28 11:18:15 +0800

    update

--- test/tank/partner_test.gd
+++ test/tank/partner_test.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Eagle.create_base()
+	Eagle.create_eagle()
 	
 	TankHelper.create_tank(TankConfig.my_tank, Vector2i(1, 10))
 	TankHelper.create_tank(TankConfig.partner_tank_1, Vector2i(28, 14))
 	TankHelper.create_tank(TankConfig.enemy_easy, Vector2i(4, 14))
+	TankHelper.create_tank(TankConfig.only_fire_enemy, Vector2i(18, 16))
 
 	TileHelper.create_tile(TileConfig.steel_wall, Vector2i(10, 15))
 	TileHelper.create_tile(TileConfig.steel_wall, Vector2i(10, 16))
