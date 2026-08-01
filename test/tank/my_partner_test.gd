b74d63961ad7d2db6e7d601ecad42ecb06a7550a
jaysunxiao <jaysunxiao@gmail.com>
2026-08-01 08:58:27 +0800

    update

--- test/tank/my_partner_test.gd
+++ test/tank/my_partner_test.gd
@@ -10,4 +10,10 @@ func _ready() -> void:
 	BuffHelper.create_buff(BuffConfig.tank_hp_buff, Vector2i(Eagle.player_tank_start_grid_pos.x, 6))
 	BuffHelper.create_buff(BuffConfig.tank_hp_buff, Vector2i(Eagle.player_tank_start_grid_pos.x, 8))
 	
+	TileHelper.create_tile(TileConfig.brick_wall, Vector2i(12, 8))
+	TileHelper.create_tile(TileConfig.steel_wall, Vector2i(14, 8))
+	TileHelper.create_tile(TileConfig.forest, Vector2i(16, 8))
+	TileHelper.create_tile(TileConfig.ice, Vector2i(18, 8))
+	TileHelper.create_tile(TileConfig.water, Vector2i(20, 8))
+	
 	pass
