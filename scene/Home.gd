093f7a81f337a5234abe008c2370a559c0e11f34
jaysunxiao <jaysunxiao@gmail.com>
2026-08-06 21:53:18 +0800

    update

--- scene/Home.gd
+++ scene/Home.gd
@@ -1,19 +1,6 @@
 extends Control
 
-const INTRO_ANIMATION := "intro"
-const TANK_FRAME_PATHS: Array[String] = [
-	"res://image/characters/blue_tank_1.png",
-	"res://image/characters/blue_tank_2.png",
-	"res://image/characters/blue_tank_3.png",
-	"res://image/characters/blue_tank_4.png",
-	"res://image/characters/blue_tank_5.png",
-	"res://image/characters/blue_tank_6.png",
-]
-const TANK_DISPLAY_SIZE := Vector2(240.0, 240.0)
-const TANK_ANIMATION_FPS := 5.0
-
-@onready var title_label: Label = $CenterContainer/VBox/HeaderRow/TitleLabel
-@onready var tank_sprite: AnimatedSprite2D = $CenterContainer/VBox/HeaderRow/TankArea/TankSprite
+@onready var title_label: Label = $CenterContainer/VBox/TitleLabel
 @onready var menu_buttons: VBoxContainer = $CenterContainer/VBox/MenuButtons
 @onready var continue_button: Button = $CenterContainer/VBox/MenuButtons/ContinueButton
 @onready var ai_mode_button: Button = $CenterContainer/VBox/MenuButtons/AiModeButton
@@ -36,25 +23,8 @@ func _ready() -> void:
 	human_mode_button.mouse_entered.connect(on_button_hover)
 	tank_agent_button.mouse_entered.connect(on_button_hover)
 	exit_button.mouse_entered.connect(on_button_hover)
-	setup_tank_animation()
 	play_intro()
 	Audio.play_voice(AudioConfig.BGM_OPENING_DEMO_PART2)
-	pass
-
-
-func setup_tank_animation() -> void:
-	var frames := SpriteFrames.new()
-	if not frames.has_animation(INTRO_ANIMATION):
-		frames.add_animation(INTRO_ANIMATION)
-	frames.set_animation_speed(INTRO_ANIMATION, TANK_ANIMATION_FPS)
-	frames.set_animation_loop(INTRO_ANIMATION, true)
-
-	for path in TANK_FRAME_PATHS:
-		frames.add_frame(INTRO_ANIMATION, load(path))
-
-	tank_sprite.sprite_frames = frames
-	var texture_size := (frames.get_frame_texture(INTRO_ANIMATION, 0) as Texture2D).get_size()
-	tank_sprite.scale = TANK_DISPLAY_SIZE / texture_size
 	pass
 
 
@@ -69,7 +39,6 @@ func play_intro() -> void:
 	title_tween.tween_property(title_label, "scale", Vector2.ONE, 0.8) \
 		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
-	tank_sprite.play(INTRO_ANIMATION)
 	await title_tween.finished
 	show_menu_buttons()
 	pass
